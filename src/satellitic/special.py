d5914216b798fdc331dfafa69eeb3e99179cd1bf
Richard Tjörnhammar <richard.tjornhammar@pts.se>
2026-03-11 12:52:53 +0100

    Update special.py with morton coding

--- src/satellitic/special.py
+++ src/satellitic/special.py
@@ -13,6 +13,16 @@ lic_ = """
    See the License for the specific language governing permissions and
    limitations under the License.
 """
+try :
+        import jax
+        jax.config.update("jax_enable_x64", True)
+        bUseJax = True
+except ImportError :
+        bUseJax = False
+except OSError:
+        bUseJax = False
+
+
 import numpy as np
 def strings_find(a, sub, start=0, end=None):
     """
@@ -28,6 +38,37 @@ def strings_find(a, sub, start=0, end=None):
         idx = str(s).find(sub, start, end)
         results.append(idx)
     return np.array(results)
+
+if bUseJax:
+   @jax.jit
+    def _split_by_3bits(x):
+        x &= 0x1fffff
+        x = (x | (x << 32)) & 0x1f00000000ffff
+        x = (x | (x << 16)) & 0x1f0000ff0000ff
+        x = (x | (x << 8))  & 0x100f00f00f00f00f
+        x = (x | (x << 4))  & 0x10c30c30c30c30c3
+        x = (x | (x << 2))  & 0x1249249249249249
+        return x
+
+    @jax.jit
+    def morton3D(x, y, z):
+        return (_split_by_3bits(x) |
+               (_split_by_3bits(y) << 1) |
+               (_split_by_3bits(z) << 2))
+else :
+    def _split_by_3bits(x):
+        x &= 0x1fffff
+        x = (x | (x << 32)) & 0x1f00000000ffff
+        x = (x | (x << 16)) & 0x1f0000ff0000ff
+        x = (x | (x << 8))  & 0x100f00f00f00f00f
+        x = (x | (x << 4))  & 0x10c30c30c30c30c3
+        x = (x | (x << 2))  & 0x1249249249249249
+        return x
+    
+    def morton3D(x, y, z):
+        return (_split_by_3bits(x) |
+               (_split_by_3bits(y) << 1) |
+               (_split_by_3bits(z) << 2))
 
 if __name__ == '__main__':
     a = ["hello world", "test string", "abcabc", "no match"]
